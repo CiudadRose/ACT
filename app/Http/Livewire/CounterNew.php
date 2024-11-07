c745192b78afe6915904551e97209a11f483111a
Rose Marie Ciudad <ciudadrosemarie176@gmail.com>
2024-11-08 06:43:07 +0800

    add counter

---
+++ app/Http/Livewire/CounterNew.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CounterNew extends Component
+{
+    public $number="0";
+    public function increment() {
+        $this->number++;
+    }
+    public function decrement() {
+        $this->number--;
+    }
+    public function render()
+    {
+        return view('livewire.counter-new');
+    }
+}
